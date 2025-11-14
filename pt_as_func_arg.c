02fb5651c46068a7af6c8fc330cfab7ea983e947
pragati ghatte <ghattepragati7@gmail.com>
2025-11-14 14:39:34 +0530

    call by ref

---
+++ pt_as_func_arg.c
@@ -0,0 +1,19 @@
+
+//call by refrance 
+
+#include <stdio.h>
+
+   void increment(int *p)
+   {
+       *p = (*p)+1;
+   }
+   
+   int main ()
+   {
+       int a; 
+       a=10;
+       increment(&a);
+       printf( "a = %d\n", a);
+
+    return 0;
+}
